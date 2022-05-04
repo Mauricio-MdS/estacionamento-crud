52953e40c5eea0a95131249f7eb109bc08d6e409
Mauricio-MdS <mauricio.martins.silva@gmail.com>
2022-05-04 11:47:59 -0300

    feat: botao cancelar

--- built/app.js
+++ built/app.js
@@ -2,10 +2,23 @@ import EstacionamentoController from "./controllers/estacionamento-controller.js
 const controller = new EstacionamentoController();
 const botaoRegistrar = document.querySelector(".registrar");
 const formulario = document.querySelector(".formulario");
+const inputNome = document.querySelector(".formulario__nome");
+const inputPlaca = document.querySelector(".formulario__placa");
 const inputEntrada = document.querySelector(".formulario__horario");
-function registrarVeiculo() {
+const botaoCancelar = document.querySelector(".cancelar");
+function visibilidadeFormulario() {
     botaoRegistrar.classList.toggle("invisible");
     formulario.classList.toggle("invisible");
+}
+function registrarVeiculo() {
+    visibilidadeFormulario();
     inputEntrada.value = `${new Date().getHours()}:${new Date().getMinutes()}`;
 }
+function cancelarRegistro() {
+    inputNome.value = "";
+    inputPlaca.value = "";
+    inputEntrada.value = "";
+    visibilidadeFormulario();
+}
 botaoRegistrar.onclick = registrarVeiculo;
+botaoCancelar.onclick = cancelarRegistro;
--- src/app.ts
+++ src/app.ts
@@ -3,13 +3,28 @@ import EstacionamentoController from "./controllers/estacionamento-controller.js
 const controller = new EstacionamentoController();
 const botaoRegistrar = document.querySelector(".registrar") as HTMLButtonElement;
 const formulario = document.querySelector(".formulario") as HTMLFormElement;
+const inputNome = document.querySelector(".formulario__nome") as HTMLInputElement;
+const inputPlaca = document.querySelector(".formulario__placa") as HTMLInputElement;
 const inputEntrada = document.querySelector(".formulario__horario") as HTMLInputElement;
+const botaoCancelar = document.querySelector(".cancelar") as HTMLButtonElement;
 
-function registrarVeiculo(): void{
+
+function visibilidadeFormulario(): void{
     botaoRegistrar.classList.toggle("invisible");
     formulario.classList.toggle("invisible");
+}
+
+function registrarVeiculo(): void{
+    visibilidadeFormulario();
     inputEntrada.value= `${new Date().getHours()}:${new Date().getMinutes()}`;
 }
 
+function cancelarRegistro(): void{
+    inputNome.value="";
+    inputPlaca.value="";
+    inputEntrada.value="";
+    visibilidadeFormulario();
+}
 
 botaoRegistrar.onclick = registrarVeiculo;
+botaoCancelar.onclick = cancelarRegistro;
